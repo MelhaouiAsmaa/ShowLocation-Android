4d61cbef6266dee6cbaa919a6407e58b9662e56f
Asmaa Melhaoui <melhaouiasmaa@gmail.com>
2020-10-29 20:23:08 +0100

    getcurrentlocation

---
+++ app/src/main/java/com/example/mygetcurrentapplocationapplication/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.mygetcurrentapplocationapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    // initialize variable
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign variable
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        //initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //check permission
+        if(ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+         //when persmission granted call method
+         //call method
+         getCurrentLocation() ;
+        }else
+        {
+            //when permission denied
+            //request permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private void getCurrentLocation() {
+
+        //initialize task location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                //when success
+                if( location != null)
+                {
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //Initialize lat lng
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            //create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("You are here");
+                            //Zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            //add marker on map
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 44)
+        {
+            if(grantResults.length > 0 && grantResults[0] ==  PackageManager.PERMISSION_GRANTED)
+            {
+                //when permission granted
+                //call method
+                getCurrentLocation();
+            }
+        }
+    }
+}
